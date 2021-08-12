9ccb261bf9054c1c6d7bacbef817b2df8b6127d9
Kaiwen Xu <kaiwenxu@usc.edu>
2021-08-12 10:46:54 -0700

    Added postgres

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -7,8 +9,12 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config["SQLALCHEMY_TRACK_NOTIFICATIONS"] = False
 api = Api(app)
 app.secret_key = 'secret'
